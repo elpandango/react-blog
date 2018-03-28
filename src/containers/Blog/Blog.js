2067cb1b1c71cb3920dccbf905d97dc164f24dec
Spider <spider.netpeak@gmail.com>
2018-03-28 15:02:59 +0300

    Blog post adding and deleting. Error catching

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 import Post from '../../components/Post/Post';
@@ -8,42 +8,58 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
-       axios.get('https://jsonplaceholder.typicode.com/posts')
-           .then(response => {
-               const posts = response.data.slice(0, 4);
-               const updatedPosts = posts.map(post => {
-                   return {
-                       ...post,
-                       author: 'Max'
-                   }
-               });
-               this.setState({posts: updatedPosts});
-               // console.log(response);
-           });
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log(response);
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true})
+            });
     }
 
-    render () {
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
 
-        const posts = this.state.posts.map(post => {
-            return <Post
-                title={post.title}
-                author={post.author}
-                key={post.id}/>
-        })
+    render() {
+
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    title={post.title}
+                    author={post.author}
+                    key={post.id}
+                    clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
